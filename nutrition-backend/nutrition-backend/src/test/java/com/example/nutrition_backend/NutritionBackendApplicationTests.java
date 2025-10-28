9ba6740700f05421d1cd99bc7d5879a267342b08
Ngô Quốc Anh <anh.nq01@mor.com.vn>
2025-10-28 18:27:38 +0700

    docker +databaseconfig

---
+++ nutrition-backend/nutrition-backend/src/test/java/com/example/nutrition_backend/NutritionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nutrition_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
