adca390b7a7c11402247961b9450f57eb31e9903
anhnq2710 <zuocwanh1511@gmail.com>
2026-01-08 22:56:59 +0700

    update giá trị dinh dưỡng mới

---
+++ nutrition-backend/nutrition-backend/src/main/resources/db/migration/V14__change_nutrtion_food_data.sql
@@ -0,0 +1,202 @@
+-- PHỞ BÒ
+UPDATE food_nutrition SET
+                          calories=90, protein=6.0, fat=2.5, saturated_fat=1.0,
+                          carbs=10.5, sugar=1.0, fiber=0.6,
+                          sodium=400, potassium=150, cholesterol=12
+WHERE name='Phở bò';
+
+-- BÚN CHẢ
+UPDATE food_nutrition SET
+                          calories=120, protein=7.5, fat=5.0, saturated_fat=1.8,
+                          carbs=12.0, sugar=1.2, fiber=0.8,
+                          sodium=450, potassium=180, cholesterol=20
+WHERE name='Bún chả';
+
+-- BÁNH MÌ PATE
+UPDATE food_nutrition SET
+                          calories=150, protein=6.0, fat=8.0, saturated_fat=3.0,
+                          carbs=16.0, sugar=1.5, fiber=1.0,
+                          sodium=350, potassium=120, cholesterol=30
+WHERE name='Bánh mì pate';
+
+-- CƠM TẤM
+UPDATE food_nutrition SET
+                          calories=150, protein=6.5, fat=5.0, saturated_fat=1.8,
+                          carbs=18.0, sugar=0.5, fiber=0.6,
+                          sodium=300, potassium=120, cholesterol=25
+WHERE name='Cơm tấm';
+
+-- BÁNH XÈO
+UPDATE food_nutrition SET
+                          calories=180, protein=5.0, fat=10.0, saturated_fat=3.5,
+                          carbs=16.0, sugar=0.5, fiber=1.2,
+                          sodium=350, potassium=150, cholesterol=40
+WHERE name='Bánh xèo';
+
+-- HỦ TIẾU
+UPDATE food_nutrition SET
+                          calories=85, protein=5.0, fat=2.0, saturated_fat=0.7,
+                          carbs=11.0, sugar=0.8, fiber=0.5,
+                          sodium=380, potassium=160, cholesterol=10
+WHERE name='Hủ tiếu';
+
+-- CHÈ BA MÀU
+UPDATE food_nutrition SET
+                          calories=140, protein=2.0, fat=3.0, saturated_fat=1.2,
+                          carbs=26.0, sugar=18.0, fiber=0.8,
+                          sodium=20, potassium=90, cholesterol=5
+WHERE name='Chè ba màu';
+
+-- BÒ KHO
+UPDATE food_nutrition SET
+                          calories=160, protein=14.0, fat=8.0, saturated_fat=3.0,
+                          carbs=6.0, sugar=1.0, fiber=0.5,
+                          sodium=420, potassium=280, cholesterol=45
+WHERE name='Bò kho';
+
+-- CÁ KHO TỘ
+UPDATE food_nutrition SET
+                          calories=160, protein=18.0, fat=7.0, saturated_fat=2.0,
+                          carbs=2.0, sugar=0.5, fiber=0.0,
+                          sodium=700, potassium=300, cholesterol=55
+WHERE name='Cá kho tộ';
+
+-- GỎI CUỐN
+UPDATE food_nutrition SET
+                          calories=70, protein=4.0, fat=2.0, saturated_fat=0.4,
+                          carbs=9.0, sugar=0.5, fiber=1.0,
+                          sodium=200, potassium=120, cholesterol=8
+WHERE name='Gỏi cuốn';
+
+-- NEM NƯỚNG
+UPDATE food_nutrition SET
+                          calories=180, protein=10.0, fat=12.0, saturated_fat=4.0,
+                          carbs=8.0, sugar=1.0, fiber=0.5,
+                          sodium=500, potassium=200, cholesterol=50
+WHERE name='Nem nướng';
+
+-- BÁNH CANH
+UPDATE food_nutrition SET
+                          calories=90, protein=4.0, fat=2.0, saturated_fat=0.6,
+                          carbs=12.0, sugar=0.6, fiber=0.5,
+                          sodium=350, potassium=150, cholesterol=8
+WHERE name='Bánh canh';
+
+-- CHÁO GÀ
+UPDATE food_nutrition SET
+                          calories=70, protein=6.0, fat=2.0, saturated_fat=0.6,
+                          carbs=8.0, sugar=0.3, fiber=0.5,
+                          sodium=250, potassium=130, cholesterol=20
+WHERE name='Cháo gà';
+
+-- BÁNH CUỐN
+UPDATE food_nutrition SET
+                          calories=110, protein=4.0, fat=3.0, saturated_fat=1.0,
+                          carbs=15.0, sugar=0.5, fiber=0.7,
+                          sodium=300, potassium=100, cholesterol=15
+WHERE name='Bánh cuốn';
+
+-- MÌ QUẢNG
+UPDATE food_nutrition SET
+                          calories=120, protein=7.0, fat=4.0, saturated_fat=1.2,
+                          carbs=14.0, sugar=0.8, fiber=1.0,
+                          sodium=350, potassium=180, cholesterol=20
+WHERE name='Mì Quảng';
+
+-- BÚN RIÊU
+UPDATE food_nutrition SET
+                          calories=95, protein=6.0, fat=3.0, saturated_fat=1.0,
+                          carbs=10.0, sugar=0.6, fiber=0.6,
+                          sodium=420, potassium=170, cholesterol=15
+WHERE name='Bún riêu';
+
+-- CÀ PHÊ SỮA ĐÁ
+UPDATE food_nutrition SET
+                          calories=60, protein=1.5, fat=2.0, saturated_fat=1.2,
+                          carbs=9.0, sugar=8.0, fiber=0.0,
+                          sodium=40, potassium=90, cholesterol=10
+WHERE name='Cà phê sữa đá';
+
+-- BÁNH FLAN
+UPDATE food_nutrition SET
+                          calories=160, protein=4.0, fat=6.0, saturated_fat=3.5,
+                          carbs=22.0, sugar=20.0, fiber=0.0,
+                          sodium=60, potassium=100, cholesterol=120
+WHERE name='Bánh flan';
+
+-- GỎI GÀ
+UPDATE food_nutrition SET
+                          calories=110, protein=12.0, fat=4.0, saturated_fat=1.0,
+                          carbs=4.0, sugar=0.5, fiber=0.8,
+                          sodium=280, potassium=220, cholesterol=40
+WHERE name='Gỏi gà';
+
+-- BÁNH TÉT
+UPDATE food_nutrition SET
+                          calories=200, protein=4.0, fat=7.0, saturated_fat=3.0,
+                          carbs=30.0, sugar=1.0, fiber=1.5,
+                          sodium=300, potassium=140, cholesterol=10
+WHERE name='Bánh tét';
+
+-- BÚN MỌC
+UPDATE food_nutrition SET
+                          calories=90, protein=5.0, fat=2.0, saturated_fat=0.7,
+                          carbs=12.0, sugar=0.5, fiber=0.5,
+                          sodium=380, potassium=150, cholesterol=10
+WHERE name='Bún mọc';
+
+-- CƠM CHIÊN
+UPDATE food_nutrition SET
+                          calories=180, protein=5.0, fat=7.0, saturated_fat=2.5,
+                          carbs=24.0, sugar=0.5, fiber=0.6,
+                          sodium=400, potassium=110, cholesterol=30
+WHERE name='Cơm chiên';
+
+-- BÁNH MÌ THỊT
+UPDATE food_nutrition SET
+                          calories=165, protein=7.0, fat=7.0, saturated_fat=2.8,
+                          carbs=18.0, sugar=1.0, fiber=1.2,
+                          sodium=420, potassium=140, cholesterol=35
+WHERE name='Bánh mì thịt';
+
+-- NEM CUA BỂ
+UPDATE food_nutrition SET
+                          calories=200, protein=8.0, fat=12.0, saturated_fat=4.0,
+                          carbs=14.0, sugar=0.5, fiber=0.8,
+                          sodium=450, potassium=160, cholesterol=45
+WHERE name='Nem cua bể';
+
+-- BÁNH TRÁNG NƯỚNG
+UPDATE food_nutrition SET
+                          calories=280, protein=8.0, fat=15.0, saturated_fat=5.5,
+                          carbs=26.0, sugar=2.0, fiber=1.0,
+                          sodium=600, potassium=180, cholesterol=60
+WHERE name='Bánh tráng nướng';
+
+-- CƠM GÀ
+UPDATE food_nutrition SET
+                          calories=150, protein=8.0, fat=5.0, saturated_fat=1.5,
+                          carbs=18.0, sugar=0.3, fiber=0.6,
+                          sodium=280, potassium=180, cholesterol=35
+WHERE name='Cơm gà';
+
+-- BÚN BÒ HUẾ
+UPDATE food_nutrition SET
+                          calories=100, protein=6.5, fat=3.5, saturated_fat=1.2,
+                          carbs=11.0, sugar=0.8, fiber=0.6,
+                          sodium=600, potassium=200, cholesterol=15
+WHERE name='Bún bò Huế';
+
+-- BÁNH KHOÁI
+UPDATE food_nutrition SET
+                          calories=190, protein=5.0, fat=10.0, saturated_fat=3.8,
+                          carbs=18.0, sugar=0.6, fiber=1.0,
+                          sodium=380, potassium=150, cholesterol=35
+WHERE name='Bánh khoái';
+
+-- BÁNH ÍT LÁ GAI
+UPDATE food_nutrition SET
+                          calories=210, protein=3.0, fat=6.0, saturated_fat=2.5,
+                          carbs=36.0, sugar=16.0, fiber=1.2,
+                          sodium=60, potassium=120, cholesterol=10
+WHERE name='Bánh ít lá gai';
